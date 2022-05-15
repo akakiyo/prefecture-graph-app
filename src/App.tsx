40adb2729d0f53f9fe8ec15192fdb8b365f10e4e
kiyo <kiyo.a328@gmail.com>
2022-05-15 21:37:03 +0900

    add:各コンポーネントにindex.tsの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "./components/Layout/Layout";
-import Graph from "./routes/Graph/Graph";
+import Layout from "./components/Layout";
+import Graph from "./routes/Graph";
 
 function App() {
   return (
