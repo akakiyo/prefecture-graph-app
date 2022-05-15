db180d8d24f43fea5b2fe4973a1e03b8d7a689f5
kiyo <kiyo.a328@gmail.com>
2022-05-15 21:08:36 +0900

    add:Bodyのファイルの追加

---
+++ src/components/Layout/Body/Body.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Body = (): JSX.Element => {
+  return <Wrapper>Header</Wrapper>;
+};
+const Wrapper = styled.div``;
+
+export default Body;
