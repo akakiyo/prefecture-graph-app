6d9a60c72f3f816148b8f2bb2cbfeea1b409f7c2
kiyo <kiyo.a328@gmail.com>
2022-05-15 21:20:58 +0900

    add:Graphファイルの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
+import Graph from "./routes/Graph/Graph";
 
 function App() {
-  return <Layout>都道府県のグラフアプリ</Layout>;
+  return (
+    <Layout>
+      <Graph />
+    </Layout>
+  );
 }
 
 export default App;
