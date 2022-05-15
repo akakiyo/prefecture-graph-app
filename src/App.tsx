676243ae443536aacc74449b90a065aa0dfdada1
kiyo <kiyo.a328@gmail.com>
2022-05-15 22:01:24 +0900

    update:Headerにタイトル、ページタイトルを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Graph from "./routes/Graph";
 
 function App() {
   return (
     <Layout>
+      <Routes>
+        <Route path="/" element={<Graph />} />
+      </Routes>
       <Graph />
     </Layout>
   );
