c1d9d89d66104835eecb1675aa188370e19f1c17
kiyo <kiyo.a328@gmail.com>
2022-05-16 09:09:20 +0900

    add:都道府県の一覧を取得

--- src/routes/Graph/Graph.tsx
+++ src/routes/Graph/Graph.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios, { AxiosRequestConfig, AxiosError } from "axios";
+import { Pref } from "../../types/Pref";
 
 const Graph = (): JSX.Element => {
-  return <Wrapper>グラフ</Wrapper>;
+  const [prefectureList, setPrefectureList] = useState<Array<Pref>>([]);
+  const getPrefectureList = async () => {
+    const getPrefectureListOption: AxiosRequestConfig = {
+      url: "https://opendata.resas-portal.go.jp/api/v1/prefectures",
+      method: "GET",
+      headers: { "X-API-KEY": `${process.env.REACT_APP_RESAS_API_KEY}` },
+    };
+    type ErrorResponse = {
+      error: string;
+    };
+    await axios(getPrefectureListOption)
+      .then((res) => {
+        setPrefectureList(res.data.result);
+      })
+      .catch((e: AxiosError<ErrorResponse>) => {
+        console.error(e);
+      });
+  };
+  useEffect(() => {
+    getPrefectureList();
+  }, []);
+
+  return (
+    <Wrapper>
+      {prefectureList.map((prefecture: Pref): JSX.Element => {
+        return <>{prefecture.prefName}</>;
+      })}
+    </Wrapper>
+  );
 };
 const Wrapper = styled.div``;
 
