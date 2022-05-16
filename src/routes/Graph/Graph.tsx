a415acaf7085e09f1821088ccf23906b0d624411
kiyo <kiyo.a328@gmail.com>
2022-05-16 18:44:24 +0900

    add:RESAS APIから取得したデータをもとにグラフを描画する機能の追加

--- src/routes/Graph/Graph.tsx
+++ src/routes/Graph/Graph.tsx
@@ -11,12 +11,42 @@ const options = {
     background: "white", //chartの背景色
   },
   xaxis: {
+    title: {
+      text: "年度",
+      offsetX: 300,
+      offsetY: -10,
+      style: {
+        color: "#000000",
+      },
+    },
     categories: [1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045],
+  },
+  yaxis: {
+    title: {
+      text: "人口数",
+      rotate: 0,
+      offsetX: 0,
+      offsetY: -220,
+      style: {
+        color: "#000000",
+      },
+    },
+  },
+  legend: {
+    position: "right",
+    offsetX: -40,
+    offsetY: 20,
+    showForSingleSeries: true,
+    itemMargin: {
+      horizontal: 40,
+      vertical: 0,
+    },
   },
 };
 const Graph = (): JSX.Element => {
   const [prefectureList, setPrefectureList] = useState<Array<Pref>>([]);
   const [displayPrefList, setDisplayPrefList] = useState<Array<DisplayPref>>([]);
+
   const getPrefectureList = async () => {
     const getPrefectureListOption: AxiosRequestConfig = {
       url: "https://opendata.resas-portal.go.jp/api/v1/prefectures",
@@ -41,8 +71,8 @@ const Graph = (): JSX.Element => {
 
   return (
     <Wrapper>
-      {prefectureList.map((prefecture: Pref): JSX.Element => {
-        return (
+      {prefectureList.map(
+        (prefecture: Pref): JSX.Element => (
           <PrefCheckBox
             key={prefecture.prefCode}
             prefCode={prefecture.prefCode}
@@ -50,12 +80,15 @@ const Graph = (): JSX.Element => {
             displayPrefList={displayPrefList}
             setDisplayPrefList={setDisplayPrefList}
           />
-        );
-      })}
-      <Chart options={options} series={displayPrefList} width="700" type="line" />
+        )
+      )}
+      {displayPrefList.length !== 0 && <StyledChart options={options} series={displayPrefList} type="line" />}
     </Wrapper>
   );
 };
 const Wrapper = styled.div``;
-
+const StyledChart = styled(Chart)`
+  width: 800px;
+  margin: 4em auto;
+`;
 export default Graph;
--- src/routes/Graph/PrefCheckBox.tsx
+++ src/routes/Graph/PrefCheckBox.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import styled from "styled-components";
 import axios, { AxiosRequestConfig, AxiosError } from "axios";
 import { DisplayPref } from "../../types/Pref";
@@ -12,32 +12,40 @@ type PropsType = {
 type ErrorResponse = {
   error: string;
 };
-type ResData = {
+type ResPopulationDataType = {
   value: number;
   year: number;
 };
 const PrefCheckBox = (props: PropsType): JSX.Element => {
   const { prefCode, prefName, displayPrefList, setDisplayPrefList } = props;
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const getPopulationOnePrefOption: AxiosRequestConfig = {
-      url: "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
-      method: "GET",
-      headers: { "X-API-KEY": `${process.env.REACT_APP_RESAS_API_KEY}` },
-      params: { prefCode: event.target.value },
-    };
-    axios(getPopulationOnePrefOption)
-      .then((res) => {
-        const values = res.data.result.data[0].data.map((data: ResData) => data["value"]);
-        setDisplayPrefList([...displayPrefList, { name: prefName, data: values }]);
-      })
-      .catch((error: AxiosError<ErrorResponse>) => {
-        console.error(error);
-      });
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const handleChange = () => {
+    if (isChecked) {
+      setDisplayPrefList(displayPrefList.filter((displayPref: DisplayPref) => displayPref.name !== prefName));
+      setIsChecked(!isChecked);
+    } else {
+      const getPopulationOnePrefOption: AxiosRequestConfig = {
+        url: "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
+        method: "GET",
+        headers: { "X-API-KEY": `${process.env.REACT_APP_RESAS_API_KEY}` },
+        params: { prefCode: prefCode },
+      };
+      axios(getPopulationOnePrefOption)
+        .then((res) => {
+          const values = res.data.result.data[0].data.map((data: ResPopulationDataType) => data["value"]);
+          setDisplayPrefList([...displayPrefList, { name: prefName, data: values }]);
+        })
+        .catch((error: AxiosError<ErrorResponse>) => {
+          console.error(error);
+        });
+      setIsChecked(!isChecked);
+    }
   };
 
   return (
     <Wrapper>
-      <input type="checkbox" placeholder="-" value={prefCode} onChange={(event) => handleChange(event)} />
+      <input type="checkbox" placeholder="-" onChange={() => handleChange()} />
       {prefName}
     </Wrapper>
   );
